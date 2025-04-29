230f5239f97f495a7ce0bb0a53e08de8758d0499
Ockey12 <haloupgarou@gmail.com>
2025-04-29 13:12:16 +0900

    create project

---
+++ FinderColumnClone/FinderColumnClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinderColumnClone
+//
+//  Created by Ockey on 2025/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinderColumnClone/FinderColumnClone/FinderColumnCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderColumnCloneApp.swift
+//  FinderColumnClone
+//
+//  Created by Ockey on 2025/04/29.
+//
+
+import SwiftUI
+
+@main
+struct FinderColumnCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
